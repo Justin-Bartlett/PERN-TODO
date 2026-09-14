4e9c1423dec4dec59803801e75bd5b73389114a3
Justin-Bartlett <finnsquib@gmail.com>
2026-09-14 16:13:52 +0100

    UPDATE AND DELETE implemented

--- server/database.sql
+++ server/database.sql
@@ -1,6 +1,6 @@
 CREATE DATABASE perntodo;
 
 CREATE TABLE  todo(
-    todo_id SERIAL PRIMARY KEY,
+    todo_id SERIAL PRIMARY KEY, -- SERIAL auto increments the PRIMARY KEY here
     description VARCHAR(255)
 );
