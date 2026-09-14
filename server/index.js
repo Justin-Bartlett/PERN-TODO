48cc93f39bd2a216db106609ebee62237780d1ee
Justin-Bartlett <finnsquib@gmail.com>
2026-09-14 15:36:01 +0100

    POST & GET implemented

--- server/index.js
+++ server/index.js
@@ -1,10 +1,41 @@
 const express = require("express")
 const app = express()
 const cors = require("cors")
+const pool = require("./db")
 
 // middleware
 app.use(cors())
 app.use(express.json())
+
+// ROUTES
+
+// create a todo
+app.post("/todos", async (req, res) => {
+  try {
+    const { description } = req.body
+    const newTodo = await pool.query(
+      "INSERT INTO todo (description) VALUES($1) RETURNING *",
+      [description],
+    )
+
+    res.json(newTodo.rows[0])
+  } catch (error) {
+    console.error(error.message)
+  }
+})
+
+// get all todos
+app.get("/todos", async () => {
+  try {
+    const allTodos = await pool.query("SELECT * FROM todo")
+  } catch (error) {
+    console.error(error.message)
+  }
+})
+
+// update a todo
+
+// delete a todo
 
 app.listen(5001, () => {
   console.log("server has started on port 5001")
